f7d34ca72896b747c2650a2d78efabf1e099ce5d
Albert <vua7@mymacewan.ca>
2020-11-02 00:43:50 -0700

    Busy weekdays and weekends are added to month class.

--- app/src/main/java/com/example/a395_team_ritchie/Month.java
+++ app/src/main/java/com/example/a395_team_ritchie/Month.java
@@ -28,7 +28,7 @@ public class Month {
             n = 31;
             monthNum = 1;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("february")){
             monthNum = 2;
@@ -37,67 +37,67 @@ public class Month {
             else
                 n = 28;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("march")){
             monthNum = 3;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("april")){
             monthNum = 4;
             n = 30;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("may")){
             monthNum = 5;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("june")){
             monthNum = 6;
             n = 30;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("july")){
             monthNum = 7;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("august")){
             monthNum = 8;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("september")){
             monthNum = 9;
             n = 30;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("october")){
             monthNum = 10;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
         else if(this.month.toLowerCase().equals("november")){
             monthNum = 11;
             n = 30;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
-        else{
+        else{ //"december"
             monthNum = 12;
             n = 31;
             shifts = new Shifts[n];
-            populateShifts();
+            populateShifts(monthNum);
         }
     }
 
@@ -105,17 +105,110 @@ public class Month {
      * This method populates the array with empty shifts based on the number of days in the month
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void populateShifts(){
-        for(int i = 0; i < n; i++){
+    private void populateShifts(int monthNum) {
+        for (int i = 0; i < n; i++) {
             LocalDate localDate = LocalDate.of(this.year, monthNum, i+1);
             DayOfWeek dayOfWeek = DayOfWeek.from(localDate);
-            if(dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7){
-                Weekend newWeekend = new Weekend();
-                shifts[i] = newWeekend;
+
+            if (monthNum == 1 && i == 0) { //January 1 (New Year)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
             }
-            else{
-                Weekday newWeekday = new Weekday();
-                shifts[i] = newWeekday;
+            else if (monthNum == 4 && i == 9) { //April 10 (Good Friday)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 5 && i == 17) { //May 18 (Victoria Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 7 && i == 0) { //July 1 (Canada Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 9 && i == 6) { //September 7 (Labour Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 10 && i == 11) { //October 12 (Thanksgiving Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 11 && i == 10) { //November 11 (Remembrance Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum ==  12 && (i >= 17 && i <= 24)) { //December 25 (Christmas Day) and the week (7 days) before
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else if (monthNum == 12 && i == 25) { //December 26 (Boxing Day)
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    BusyWeekend newBusyWeekend = new BusyWeekend();
+                    shifts[i] = newBusyWeekend;
+                }
+                else { //Weekday
+                    BusyWeekday newBusyWeekday = new BusyWeekday();
+                    shifts[i] = newBusyWeekday;
+                }
+            }
+            else { //Not a holiday
+                if (dayOfWeek.getValue() == 6 || dayOfWeek.getValue() == 7) { //Weekend
+                    Weekend newWeekend = new Weekend();
+                    shifts[i] = newWeekend;
+                }
+                else { //Weekday
+                    Weekday newWeekday = new Weekday();
+                    shifts[i] = newWeekday;
+                }
             }
         }
     }
