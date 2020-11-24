97dbec7107da1c49515b1fcd753d4f34db82244f
Albert <vua7@mymacewan.ca>
2020-11-24 15:44:29 -0700

    Made edit student screen scrollable.

--- app/src/main/java/com/example/a395_team_ritchie/StudentViewActivity.java
+++ app/src/main/java/com/example/a395_team_ritchie/StudentViewActivity.java
@@ -1,13 +1,11 @@
 package com.example.a395_team_ritchie;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,7 +16,8 @@ public class StudentViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_view);
 
-        final Button addStudent = (Button) findViewById(R.id.addStudentbutton);
+        final Button addStudent = (Button) findViewById(R.id.addStudentButtonInitial);
+        final Button updateStudent = (Button) findViewById(R.id.updateStudentButtonInitial);
         final LinearLayout layout = (LinearLayout) findViewById(R.id.studentLinear);
 
         for (int i = 0; i < 50; i++) {
@@ -34,6 +33,15 @@ public class StudentViewActivity extends AppCompatActivity {
                 Intent addStudentIntent = new Intent(getApplicationContext(), AddStudentActivity.class);
                 startActivity(addStudentIntent);
             }
+        });
+
+        updateStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent updateStudentIntent= new Intent(getApplicationContext(), EditStudentActivity.class);
+                startActivity(updateStudentIntent);
+            }
+
         });
     }
 }
